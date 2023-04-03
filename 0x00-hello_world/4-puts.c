2e5ee8fc718674c09dbb5c822879b78a3114e519
Dorraghanmy <ghanmydorra@gmail.com>
2023-04-03 15:08:31 -0700

    -4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <studio.h>
+/**
+  * main - A progam that prints a line with puts function
+  * return: 0 (Sucess)
+  */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
